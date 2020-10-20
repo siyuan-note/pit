a1f5f5e8545646328cecf4fe2acd90ba31a2b80e
Liang Ding <d@b3log.org>
2020-10-20 18:06:29 +0800

    :tada: 内核更新器

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/siyuan-note/kernel-updater
+
+go 1.15
+
+require (
+	github.com/88250/gulu v1.1.1
+	golang.org/x/text v0.3.3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,77 @@
+// SiYuan - 源于思考，饮水思源
+// Copyright (c) 2020-present, ld246.com
+//
+// 本文件属于思源笔记源码的一部分，云南链滴科技有限公司版权所有。
+//
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/88250/gulu"
+)
+
+var (
+	HomeDir, _    = gulu.OS.Home()
+	WorkingDir, _ = os.Getwd()
+	ConfDir       = filepath.Join(HomeDir, ".siyuan")
+	LogPath       = filepath.Join(ConfDir, "siyuan-update.log")
+)
+
+var (
+	Logger  *gulu.Logger
+	logFile *os.File
+)
+
+func init() {
+	if !gulu.File.IsExist(ConfDir) {
+		if err := os.Mkdir(ConfDir, 0755); nil != err && !os.IsExist(err) {
+			Logger.Fatalf("create conf folder [%s] failed: %s", ConfDir, err)
+		}
+	}
+
+	if gulu.File.IsExist(LogPath) {
+		if size := gulu.File.GetFileSize(LogPath); 1024*1024*2 <= size {
+			// 日志文件大于 2M 的话删了重建
+			if err := os.Remove(LogPath); nil != err {
+				fmt.Errorf("remove log file [%s] failed: %s", LogPath, err)
+				os.Exit(-2)
+			}
+		}
+	}
+
+	var err error
+	logFile, err = os.OpenFile(LogPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0664)
+	if nil != err {
+		fmt.Errorf("create log file [%s] failed: %s", LogPath, err)
+		os.Exit(-2)
+	}
+
+	gulu.Log.SetLevel("trace")
+	Logger = gulu.Log.NewLogger(io.MultiWriter(logFile, os.Stdout))
+}
+
+func main() {
+	defer logFile.Close()
+
+	syTempFolder := filepath.Join(os.TempDir(), "siyuan")
+	p := filepath.Join(syTempFolder, "update.zip")
+	if !gulu.File.IsExist(p) {
+		return
+	}
+
+	Logger.Infof("unzipping update pack [from=%s, to=%s]", p, WorkingDir)
+	if err := gulu.Zip.Unzip(p, WorkingDir); nil != err {
+		Logger.Errorf("unzip update pack failed: %s", err)
+		return
+	}
+	Logger.Infof("unzipped update pack")
+	if err := os.RemoveAll(p); nil != err {
+		Logger.Errorf("remove update pack [%s] failed: %s", p, err)
+		return
+	}
+}
